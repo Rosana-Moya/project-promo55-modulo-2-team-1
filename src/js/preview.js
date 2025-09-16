95c76b744af73d69ceda66feaa4c756011c47ba8
tasac-sainz <tssainzdeaja@gmail.com>
2025-09-16 12:50:25 +0200

    añadidos diseños para invitación de boda

---
+++ src/js/preview.js
@@ -0,0 +1,21 @@
+'use strict'
+console.log("ready preview");
+
+const preview = document.querySelector(".preview-container");
+const design = document.querySelector(".preview__chosen-style");
+
+const designClasses = ["autum", "minimal", "fun"];
+
+design.addEventListener ("change", (event) => {
+    const selectDesign = event.target.value;
+    preview.classList.remove(...designClasses);
+    switch (selectDesign) {
+        case "autum": preview.classList.add ("autum");
+        break;
+        case "minimal": preview.classList.add ("minimal");
+        break;
+        case "fun": preview.classList.add ("fun");
+        break;
+
+    }
+})
