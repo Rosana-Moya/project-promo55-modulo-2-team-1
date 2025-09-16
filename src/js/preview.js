be04d0343817092195878ea49046e96a37b11354
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-09-16 18:20:15 +0200

    captando elementos del form y preview antes de escuchar eventos

--- src/js/preview.js
+++ src/js/preview.js
@@ -19,14 +19,29 @@ design.addEventListener ("change", (event) => {
 
     }
 })
-const inputName = document.getElementById("name");   //solo para probar cómo escuchar el form - sin mergear no tengo acceso
+const inputName = document.getElementById("name");   
+const inputMessage = document.getElementById("message");
+const inputEmail = document.getElementById("email");
+const inputPhone = document.getElementById("phone");
 const inputDate = document.getElementById("date");
+const inputAddress = document.getElementById("address");
+const inputPhoto = document.getElementById("photo");
+
 const hostName = document.querySelector(".preview-main__host-name");
 const message = document.querySelector(".preview-main__message");
 const eventDate = document.querySelector(".preview__event-date");
+const hostMail = document.querySelector(".preview__mail");
+const hostPhone = document.querySelector(".preview__phone");
+const hostAddress = document.querySelector(".preview__address");
+
+
+
 
 inputName.addEventListener("input", (event) => {
     hostName.textContent = event.target.value;
+})
+inputMessage.addEventListener("input", (event) => {
+    message.textContent = event.target.value;
 })
 
 inputDate.addEventListener("input", (event) => {
