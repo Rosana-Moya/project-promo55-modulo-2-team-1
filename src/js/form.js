656d0df4b06b2d703efde4e046af3d26b06f8341
Karengonsan <karenglezschez@gmail.com>
2025-09-16 13:01:46 +0200

    Sprint 1 – Maquetación form

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+// ------------------------------
+// ELEMENTOS DOM
+// ------------------------------
+
+// Collapsables
+const collapsibleHeaders = document.querySelectorAll(".collapsible__header");
+const collapsibleContents = document.querySelectorAll(".collapsible__content");
+
+// Formulario - inputs
+const inputName = document.getElementById("name");
+const inputMessage = document.getElementById("message");
+const inputEmail = document.getElementById("email");
+const inputPhone = document.getElementById("phone");
+const inputDate = document.getElementById("date");
+const inputAddress = document.getElementById("address");
+const inputPhoto = document.getElementById("photo");
+
+// Sección de compartir
+const downloadButton = document.querySelector(".js-download");
+const resetButton = document.querySelector(".js-reset");
+const formSubmitButton = document.querySelector(".js-create");
