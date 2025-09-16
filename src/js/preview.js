b9cc6124d16330c45c1e893c5151034af124a41a
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-09-16 20:59:56 +0200

    quitando seccion de diseño de la preview

--- src/js/preview.js
+++ src/js/preview.js
@@ -2,23 +2,6 @@
 console.log("ready preview");
 
 const preview = document.querySelector(".preview-container");
-const design = document.querySelector(".preview__chosen-style");
-
-const designClasses = ["autum", "minimal", "minimalSerius", "fun"];
-
-design.addEventListener ("change", (event) => {
-    const selectDesign = event.target.value;
-    preview.classList.remove(...designClasses);
-    switch (selectDesign) {
-        case "autum": preview.classList.add ("autum");
-        break;
-        case "minimal": preview.classList.add ("minimal");
-        break;
-        case "fun": preview.classList.add ("fun");
-        break;
-
-    }
-})
 
 const hostName = document.querySelector(".preview-main__host-name");
 const message = document.querySelector(".preview-main__message");
