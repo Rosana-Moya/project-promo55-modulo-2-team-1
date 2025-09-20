9b1b60795e3c66d0099328a10503cc8d7acd0905
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-09-20 10:24:31 +0200

    Añadido almacenamiento en Local Storage y mantenimiento de datos al recargar la página.

--- src/js/preview.js
+++ src/js/preview.js
@@ -18,36 +18,75 @@ const fontSlider = document.querySelector(".js-font-size");
 const fontFamily = document.querySelector(".js-font");
 const fontColor = document.querySelector(".js-font-color");
 
+//CARGA LA PÁGINA CON LOS DATOS ALMACENADOS EN LOCALSTORAGE
+//Datos de cliente
+inputName.value = localStorage.getItem("hostName");
+hostName.textContent = localStorage.getItem("hostName");
+inputMessage.value = localStorage.getItem("message");
+message.textContent = localStorage.getItem("message");
+inputDate.value = localStorage.getItem("eventDate");
+eventDate.textContent = localStorage.getItem("eventDate");
+inputEmail.value = localStorage.getItem("hostMail");
+hostMail.textContent = localStorage.getItem("hostName");
+inputPhone.value = localStorage.getItem("hostPhone");
+hostPhone.textContent = localStorage.getItem("hostPhone");
+inputAddress.value = localStorage.getItem ("hostAddress");
+hostAddress.textContent = localStorage.getItem ("hostAddress");
+
+//Datos de Diseño
+previewMain.style.fontSize = localStorage.getItem("fontSize");
+preview.style.fontFamily = localStorage.getItem("fontFamily");
+preview.style.color = localStorage.getItem("fontColor");
+preview.style.backgroundImage = localStorage.getItem("selectedBackground");
+
 inputName.addEventListener("input", (event) => {
     hostName.textContent = event.target.value;
+    localStorage.setItem("hostName", event.target.value);
 })
 inputMessage.addEventListener("input", (event) => {
     message.textContent = event.target.value;
+    localStorage.setItem("message", event.target.value);
 })
 inputDate.addEventListener("input", (event) => {
     eventDate.textContent = event.target.value;
+    localStorage.setItem("eventDate", event.target.value);
 })
 inputEmail.addEventListener("input", (event) => {
     hostMail.textContent = event.target.value;
+    localStorage.setItem("hostMail", event.target.value);
 })
 inputPhone.addEventListener("input", (event) => {
     hostPhone.textContent = event.target.value;
+    localStorage.setItem("hostPhone", event.target.value);
 })
 inputAddress.addEventListener("input", (event) => {
     hostAddress.textContent = event.target.value;
+    localStorage.setItem("hostAddress", event.target.value);
 })
 
 fontSlider.addEventListener("input", () => {
     const newSize = fontSlider.value;
     previewMain.style.fontSize = newSize + "px";
+    localStorage.setItem("fontSize", newSize + "px");
 });
 
 fontFamily.addEventListener("change", (event) => {
     const selectedFont = event.target.value;
     preview.style.fontFamily = selectedFont;
+    localStorage.setItem("fontFamily", event.target.value);
 });
 
 fontColor.addEventListener("input", (event) => {
     const newColor = event.target.value;
     preview.style.color = newColor;
+    localStorage.setItem("fontColor", newColor);
 })
+
+document.addEventListener("formDataUpdated", (backgroundImage) => {
+  const data = backgroundImage.detail;
+  if (data.background) {
+    preview.style.backgroundImage = `url("${data.background}")`;
+    localStorage.setItem("selectedBackground", preview.style.backgroundImage);
+  };
+  });
+
