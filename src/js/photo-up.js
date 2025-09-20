d5f687a828b67fe8a70aad2a716cc09c67250769
Michelle-1982 <michellepachecopantoja@gmail.com>
2025-09-20 11:41:57 +0200

    actualizando

--- src/js/photo-up.js
+++ src/js/photo-up.js
@@ -4,13 +4,16 @@ console.log("ready preview");
 
 const inputImg = document.querySelector(".js_input_file");
 const photoUp = document.querySelector(".photo-up__image");
+console.log("inputImg", inputImg);
+console.log("photoUp", photoUp);
 
 inputImg.addEventListener("change", (event) =>{
  const file = event.target.files[0];  // Obtén el primer archivo seleccionado
-  if (file) {
+ console.log("Archivo seleccionado:", file);
+ if (file) {
     const reader = new FileReader();
      reader.onload = function(e) {
-      // Establece la imagen como la fuente del <img>
+      console.log("Imagen cargada:", e.target.result);
       photoUp.src = e.target.result;
       photoUp.style.display = 'block';  // Muestra la imagen en el preview
     };
